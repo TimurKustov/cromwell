842d458f06a8c4e91f27b41099bf59aed5019de4
lumen <lumen06@mail.ru>
2019-08-23 14:44:08 +0300

    Refactor: ObjectCounter, FileChecker and unit test FileCheckerSpec
    Common logic is defined at parsePath at ObjectCounter now. Also parsePath takes as argument a regex for specify filechecker, which are at FileCheckers as private fields. FileCheckerSpec moved to centaur/src/test/scala/centaur/testfilecheck

--- centaur/src/test/scala/centaur/testfilecheck/FileCheckerSpec.scala
+++ centaur/src/test/scala/centaur/testfilecheck/FileCheckerSpec.scala
@@ -1,23 +1,23 @@
-package centaur.s3filechecking
+package centaur.testfilecheck
 
 import java.util
 
-import centaur.test.AwsS3Ops
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.model.{ObjectListing, S3ObjectSummary}
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.FlatSpec
 import org.specs2.matcher.ShouldMatchers
 
-class TestAwsFileChecker extends FlatSpec with ShouldMatchers with MockFactory {
+class FileCheckerSpec extends FlatSpec with ShouldMatchers with MockFactory {
+
+  import centaur.test.ObjectCounterInstances._
 
   val amazonS3Stub = stub[AmazonS3]
   val objListing = stub[ObjectListing]
-  val awsS3Ops = AwsS3Ops(amazonS3Stub)
   val testPath = "s3://my-cool-bucket/path/to/file"
   val bucketName = "my-cool-bucket"
   val dirName = "path/to/file"
-  val awsS3Path = awsS3Ops.parsePath(testPath)
+  val awsS3Path = awsS3ObjectCounter.parsePath("^s3:\\/\\/.*")(testPath)
 
 
   "parsePath" should "return a bucket and directories" in {
@@ -33,7 +33,7 @@ class TestAwsFileChecker extends FlatSpec with ShouldMatchers with MockFactory {
 
   }
 
-  "AwsFilesChecker" should "should return 1 if the file exist else 0" in {
+  "countObjectAtPath" should "should return 1 if the file exist else 0" in {
 
     val objSummary = new S3ObjectSummary
     objSummary.setSize(1)
@@ -44,7 +44,7 @@ class TestAwsFileChecker extends FlatSpec with ShouldMatchers with MockFactory {
     (objListing.getObjectSummaries _).when().returns(summaries)
     (amazonS3Stub.listObjects(_ : String, _ : String)).when(bucketName, dirName).returns(objListing)
 
-    val isExist =  awsS3Ops.countObjectsAtPath(awsS3Path)
+    val isExist =  awsS3ObjectCounter.countObjectsAtPath(amazonS3Stub)(awsS3Path)
     assert(isExist == 1)
   }
 }
